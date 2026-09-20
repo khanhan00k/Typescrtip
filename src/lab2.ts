76c1d43511412ac4d77797e16cd10ee91944af10
Khanh An <annkth05572@gmail.com>
2026-09-21 06:19:14 +0700

    lab2

--- src/lab2.ts
+++ src/lab2.ts
@@ -1,33 +1,79 @@
-// const Product
-// enum
 enum ProductRate {
-  low = "thấp",
-  high = "cao",
+  low = "Thấp",
+  medium = "Trung bình",
+  high = "Cao"
 }
 
-const product: { name: string; rate: ProductRate; price: number } = {
-  name: "laptop",
-  rate: ProductRate.low,
-  price: 10000,
+type  Product = {
+  name: string;
+  sales: number;
+  price: number;
+  rate: ProductRate;
+  descrition?: string;
 };
 
-console.log(product);
-
-const listProducts: { name: string; rate: ProductRate; price: number }[] = [
+const listProduct: Product[] = [
   {
-    name: "laptop1",
-    rate: ProductRate.low,
-    price: 10000,
+    name: "Áo thun",
+    sales: 100,
+    price: 200000,
+    rate: ProductRate.medium,
+    descrition: "Áo thun nam nữ"
   },
   {
-    name: "laptop2",
+    name: "Quần jean",
+    sales: 50,
+    price: 300000,
     rate: ProductRate.high,
-    price: 10000,
+    descrition: "Quần jean nam nữ"
+
   },
+  {
+    name: "Giày thể thao",
+    sales: 30,
+    price: 500000,
+    rate: ProductRate.low,
+    descrition: "Giày thể thao"
+  },
+  {
+    name: "Mũ lưỡi trai",
+    sales: 80,
+    price: 150000,
+    rate: ProductRate.medium,
+    descrition: "Mũ lưỡi trai"
+  },
+  {
+    name: "Túi xách",
+    sales: 20,
+    price: 400000,
+    rate: ProductRate.high,
+    descrition: "Túi xách"
+  }
 ];
 
-function calPrice(list: { name: string; rate: ProductRate; price: number }[]) {
-  return 111;
+function hienthi(listProduct: Product[]) {
+ listProduct.forEach((product) => {
+  console.log(`Tên sản phẩm: ${product.name}`);
+  console.log(`Doanh số: ${product.sales}`);
+  console.log(`Giá: ${product.price}`);
+  console.log(`Đánh giá: ${product.rate}`);
+ });
 }
-console.log(listProducts);
-console.log(calPrice(listProducts));
+
+function Tinhtong(listProduct: Product[]){
+  return listProduct.reduce((tong, product) => tong + product.price, 0);
+}
+
+function LocSp(listProduct: Product[]){
+  return listProduct.filter((product)=>{
+      return product.sales && product.rate !==  ProductRate.low;
+  });
+}
+
+console.log("Danh Sach San Pham");
+hienthi(listProduct);
+console.log("Tổng: ", Tinhtong(listProduct));
+console.log("Lọc: ", LocSp(listProduct));
+
+
+
